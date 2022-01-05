e3849e954009804dfd0adde0b2ee415d8c9cc9b3
25111 <2511183459@qq.com>
2022-01-05 14:55:31 +0800

    添加本地缓存

--- src/main/java/com/mycoder/community/service/DiscussPostService.java
+++ src/main/java/com/mycoder/community/service/DiscussPostService.java
@@ -1,13 +1,22 @@
 package com.mycoder.community.service;
 
+import com.github.benmanes.caffeine.cache.CacheLoader;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.github.benmanes.caffeine.cache.LoadingCache;
 import com.mycoder.community.dao.DiscussPostMapper;
 import com.mycoder.community.entity.DiscussPost;
 import com.mycoder.community.util.SensitiveFilter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.util.HtmlUtils;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author cj
@@ -15,21 +24,99 @@ import java.util.List;
  */
 @Service
 public class DiscussPostService {
+
+    public static final Logger logger = LoggerFactory.getLogger(DiscussPostService.class);
+
     @Autowired
     DiscussPostMapper discussPostMapper;
 
     @Autowired
     SensitiveFilter filter;
 
+    @Value("${caffeine.posts.max-size}")
+    int maxSize;
+
+    @Value("${caffeine.posts.expire-seconds}")
+    int expireSeconds;
+
+    //帖子列表缓存
+    private LoadingCache<String, List<DiscussPost>> postListCache;
+
+    //帖子总数缓存
+    private LoadingCache<Integer, Integer> postRowsCache;
+
+
+    @PostConstruct
+    public void init(){
+        //初始化帖子列表缓存
+
+        postListCache = Caffeine.newBuilder()
+                .maximumSize(maxSize)
+                .expireAfterWrite(expireSeconds, TimeUnit.SECONDS)
+                .build(new CacheLoader<String, List<DiscussPost>>() {
+                    @Override
+                    public  List<DiscussPost> load(String key) throws Exception {
+                        if(key == null || key.length() == 0){
+                            throw new IllegalArgumentException("参数错误");
+                        }
+
+                        String[] params = key.split(":");
+                        if (params == null || params.length != 2){
+                            throw new IllegalArgumentException("参数错误");
+                        }
+                        int offset = Integer.parseInt(params[0]);
+                        int limit = Integer.parseInt(params[1]);
+
+                        //可以先访问redis
+
+                        logger.debug("从【数据库】查询帖子列表-->初始化");
+                        return discussPostMapper.selectDiscussPosts(0, offset, limit, 1);
+                    }
+                });
+
+
+        //初始化帖子总数缓存
+        postRowsCache = Caffeine.newBuilder()
+                .maximumSize(maxSize)
+                .expireAfterWrite(expireSeconds, TimeUnit.SECONDS)
+                .build(new CacheLoader<Integer, Integer>() {
+                    @Override
+                    public Integer load(Integer key) throws Exception {
+
+                        //可以先访问redis
+
+                        logger.debug("从【数据库】查询帖子行数-->初始化");
+                        return discussPostMapper.selectDiscussPostRows(key);
+                    }
+                });
+    }
+
+
     //按条件查询帖子
     public List<DiscussPost> findDiscussPosts(int userId, int offSet, int limit, int orderMode){
+        //只有查首页的热帖时，才从本地缓存中取
+        if(userId == 0 && orderMode == 1){
+            logger.debug("从【本地缓存】查询帖子列表");
+            return postListCache.get(offSet + ":" + limit);
+        }
+
+        logger.debug("从【数据库】查询帖子列表");
         return discussPostMapper.selectDiscussPosts(userId, offSet, limit, orderMode);
     }
 
+
     //按userId查询帖子数量
     public int findDiscussRows(int userId){
+        //只有查首页时，才从本地缓存中取行数，且对应的key一直为0
+        if(userId == 0){
+            logger.debug("从【本地缓存】查询帖子数量");
+            return postRowsCache.get(0);
+        }
+
+        logger.debug("从【数据库】查询帖子数量");
         return discussPostMapper.selectDiscussPostRows(userId);
     }
+
 
     //将帖子过滤后插入到表中
     public int addDiscussPost(DiscussPost discussPost){
@@ -48,9 +135,11 @@ public class DiscussPostService {
         return discussPostMapper.insertDiscussPost(discussPost);
     }
 
+
     public DiscussPost findDiscussPostById(int id){
         return discussPostMapper.selectDiscussPost(id);
     }
+
 
     public int updateCommentCount(int id, int commentCount){
         return discussPostMapper.updateCommentCount(id, commentCount);
@@ -65,6 +154,7 @@ public class DiscussPostService {
     public int updateType(int id, int type){
         return discussPostMapper.updateType(id, type);
     }
+
 
     /**更新帖子分数*/
     public int updateScore(int id, double score){
--- src/main/java/com/mycoder/community/util/CookieUtil.java
+++ src/main/java/com/mycoder/community/util/CookieUtil.java
@@ -16,11 +16,13 @@ public class CookieUtil {
         }
 
         Cookie[] cookies = request.getCookies();
-
-        for(Cookie cookie : cookies){
-            if(cookie.getName().equals(name))
-                return cookie.getValue();
+        if(cookies != null){
+            for(Cookie cookie : cookies){
+                if(cookie.getName().equals(name))
+                    return cookie.getValue();
+            }
         }
+
 
         return null;
     }
